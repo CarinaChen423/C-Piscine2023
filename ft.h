0f9d9554161a8c30dd0f9e5676f13ef49c15889d
Zhe Chen <115776828+CarinaChen423@users.noreply.github.com>
2023-12-14 18:44:15 +0100

    C08

---
+++ ft.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zhchen <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/13 19:00:36 by zhchen            #+#    #+#             */
+/*   Updated: 2023/12/13 19:06:09 by zhchen           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef FT_H
+# define FT_H
+
+void	ft_putchar(char c);
+void	ft_swap(int *a, int *b);
+void	ft_putstr(char *str);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
+
+#endif
